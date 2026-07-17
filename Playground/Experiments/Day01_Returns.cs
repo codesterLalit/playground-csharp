f1b296bca4a909bb4c4bbcffc7e1a9d101771e2d
codesterlalit <laalit.sunaar@gmail.com>
2026-07-17 16:34:06 +0100

    feat: day returns arrays

--- Playground/Experiments/Day01_Returns.cs
+++ Playground/Experiments/Day01_Returns.cs
@@ -4,13 +4,24 @@ public static class Day01_Returns
 {
     public static void Run()
     {
-        double yesterday = 100;
-        double today = 103;
+        double[] price = {100, 103, 101, 105};
+        double[] returns = SimpleReturn(price);
 
-        double simpleReturn = (today- yesterday) / yesterday;
-        double logReturn = Math.Log(today / yesterday);
+        Console.WriteLine("Daily simple returns:");
+        foreach (var r in returns)
+        {
+            Console.WriteLine(r);
+        }
+    }
 
-        Console.WriteLine($"Simple return: {simpleReturn}");
-        Console.WriteLine($"Log return: {logReturn}");
+    public static double[] SimpleReturn(double[] prices)
+    {
+        var returns = new double[prices.Length - 1];
+
+        for(int i = 1; i < prices.Length; i++)
+        {
+            returns[i-1] = (prices[i] - prices[i - 1]) / prices[i - 1];
+        }
+        return returns;
     }
 }

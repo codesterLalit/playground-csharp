6da6b7b5c3326ddc7c9ed3b6ef2a5a21e5224ac6
codesterlalit <laalit.sunaar@gmail.com>
2026-08-08 12:40:13 +0100

    feat: added services

--- Playground/Cleanarch/Ex04_ServiceLifetimes.cs
+++ Playground/Cleanarch/Ex04_ServiceLifetimes.cs
@@ -3,13 +3,18 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Play.cleanarch;
 
 public class SingletonTracker {public Guid InstanceId {get;} = Guid.NewGuid();}
-public class ScopeTracker {public Guid InstanceI{get; } = Guid.NewGuid();}
+public class ScopedTracker {public Guid InstanceI{get; } = Guid.NewGuid();}
 public class TransientTracker {public Guid InstanceId{get;} = Guid.NewGuid();}
 
 public static class Ex04_ServiceLifetimes
 {
     public static void Run()
     {
+        var services = new ServiceCollection();
+
+        services.AddSingleton<SingletonTracker>();
+        services.AddScoped<ScopedTracker>();
+        services.AddTransient<TransientTracker>();
 
     }
 }
